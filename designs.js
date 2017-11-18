fff2b806f855a571091e1026948c595080caf945
readm2 <readm2@readm2ui-MacBook-Pro.local>
2017-11-18 20:09:51 +0900

    Modify codes and bugs from received reviews

--- designs.js
+++ designs.js
@@ -1,25 +1,38 @@
 // Select color input
 // Select size input
+let inputHeight = null;
+let inputWidth = null;
+let inputColor = null;
+
+window.onload = function() {
+    //  window onload, put each elements in to each variables to use later.
+    inputHeight = document.querySelector('#input_height');
+    inputWidth = document.querySelector('#input_width');
+    inputColor = document.querySelector('#colorPicker');
+};
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
     console.log('::::makeGrid');
 
-// Your code goes here!
-    var height = document.querySelector('#input_height').value;
-    var width = document.querySelector('#input_width').value;
-    var color = document.querySelector('#colorPicker').value;
+    // save input height and width values from user
+    let height = inputHeight.value;
+    let width = inputWidth.value;
 
-    var table = document.querySelector('#pixel_canvas');
+    // render table cells
+    let table = document.querySelector('#pixel_canvas');
+
+    // clear table before rendering cells
     table.innerHTML = '';
 
-    for(var i=0; i<height; i++) {
-        var tr = document.createElement('tr');
-        for(var j=0; j<width; j++) {
-            var td = document.createElement('td');
+    // rendering cells
+    for(let i=0; i<height; i++) {
+        let tr = document.createElement('tr');
+        for(let j=0; j<width; j++) {
+            let td = document.createElement('td');
+            // add 'click' event for filling the selected color
             td.addEventListener('click', function() {
-                this.style.background = color;
+                this.style.background = inputColor.value;
             });
             tr.appendChild(td);
         }
